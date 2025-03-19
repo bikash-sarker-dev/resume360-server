3845d1ec6f6ea6465d99516f3f9b8b04e550710f
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-19 15:46:20 +0600

    the mongose install and connection in application server

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = process.env.SERVER_PORT || 5000;
+
+// mongodb connection
+const dataBaseUrl = process.env.DB_CONNECT_URL;
+
+mongoose
+  .connect(dataBaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("The mongoDb connection successfully.");
+  })
+  .catch((error) => {
+    console.log("MongoDb connection error:", error);
+  });
+
+// middleware use
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("the resume 360 server open.");
