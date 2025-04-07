a5e28aa9420409206865ffd51c68a05c82200712
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-04-07 12:24:10 +0600

    the crate schema type catch and api route post

--- model_schemas/resumeSchema.js
+++ model_schemas/resumeSchema.js
@@ -2,7 +2,75 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const createResumeSchema = new Schema({});
+const createResumeSchema = new Schema({
+  personalInfo: {
+    fullName: String,
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: String,
+    address: String,
+    jobTitle: String,
+    profileImage: String,
+    imageName: String,
+    about: String,
+    date: {
+      type: Date,
+    },
+    gender: String,
+    nationality: String,
+  },
+  education: [
+    {
+      school: String,
+      degree: String,
+      field: String,
+      startDate: {
+        type: Date,
+        required: true,
+      },
+      endDate: {
+        type: Date,
+        required: true,
+      },
+      grade: String,
+      description: String,
+    },
+  ],
+  skills: [String],
+  socialLinks: [
+    {
+      platform: String,
+      link: String,
+    },
+  ],
+  projects: [
+    {
+      projectName: String,
+      description: String,
+      live: String,
+      client: String,
+      server: String,
+      features: [String],
+    },
+  ],
+  experience: [
+    {
+      company: String,
+      position: String,
+      startMonth: String,
+      endMonth: String,
+      description: String,
+    },
+  ],
+  languages: [
+    {
+      language: String,
+      proficiency: String,
+    },
+  ],
+});
 
 const resumeInfo = mongoose.model("ResumeInfo", createResumeSchema);
 
--- resumeHandle/resumeHandler.js
+++ resumeHandle/resumeHandler.js
@@ -2,6 +2,22 @@ const express = require("express");
 const resumeRouter = express.Router();
 const resumeInfo = require("../model_schemas/resumeSchema.js");
 
+// post resume working
+resumeRouter.post("/", async (req, res) => {
+  const resumeInformation = req.body;
+  const resumeModel = new resumeInfo(resumeInformation);
+  await resumeModel
+    .save()
+    .then((result) => {
+      console.log("resume save");
+      res.status(200).send({ message: "resume information save", result });
+    })
+    .catch((error) => {
+      console.log("resume ", error);
+      res.status(500).send({ message: "resume information save error", error });
+    });
+});
+
 resumeRouter.get("/", async (req, res) => {
   res.send({ message: "test successfully" });
 });
