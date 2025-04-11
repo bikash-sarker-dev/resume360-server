a85ba15c20ecf9ff368ed0286ce4080bef5b1f2a
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-04-11 10:53:13 +0600

    the profile image create api post , update, get

--- profileImageMange/profileImageHandle.js
+++ profileImageMange/profileImageHandle.js
@@ -2,68 +2,68 @@ const express = require("express");
 const profileImageRouter = express.Router();
 const profileImage = require("../model_schemas/profileImageSchema");
 
-profileImage.get("/", async (req, res) => {
-  res.send({ message: "test profile image route backend" });
+// profileImageRouter.get("/", async (req, res) => {
+//   res.send({ message: "test profile image route backend" });
+// });
+
+// post profile image related working
+profileImageRouter.post("/", async (req, res) => {
+  const imageData = req.body;
+  let imageModel = new profileImage(imageData);
+  await imageModel
+    .save()
+    .then((result) => {
+      res
+        .status(200)
+        .send({ message: " Successfully profile update", status: 200 });
+    })
+    .catch((err) => {
+      res.status(500).send({ massage: "Error Saving user:", error });
+    });
 });
 
-// post profile related working
-// profileImage.post("/", async (req, res) => {
-//   const profileInfo = req.body;
+// update profile image related working
+profileImageRouter.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  let updateImgData = req.body;
+  const query = { _id: id };
+  let updateInfo = {
+    $set: {
+      ...updateImgData,
+    },
+  };
+  await profileImage
+    .updateOne(query, updateInfo)
+    .then((result) => {
+      res.status(200).send({
+        message: " successfully profile image update and save",
+        status: 200,
+        result,
+      });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "profile image update save error", error });
+    });
+});
 
-//   let profileModel = new profiles(profileInfo);
-//   await profileModel
-//     .save()
-//     .then((result) => {
-//       res
-//         .status(200)
-//         .send({ message: " Successfully profile update", status: 200 });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({ massage: "Error Saving user:", error });
-//     });
-// });
+// get profile image
+profileImageRouter.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: id };
+    let result = await profileImage.findOne(query);
+    res.status(200).send({
+      message: "the profile image get single successfully ",
+      status: 200,
+      result,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "the profile data get single data error ", err });
+  }
+});
 
-// update profile working
-// profileImage.put("/:id", async (req, res) => {
-//   const id = req.params.id;
-//   let updateData = req.body;
-//   const query = { _id: id };
-//   let updateInfo = {
-//     $set: {
-//       ...updateData,
-//     },
-//   };
-//   await profiles
-//     .updateOne(query, updateInfo)
-//     .then((result) => {
-//       res.status(200).send({
-//         message: " successfully profile update and save",
-//         status: 200,
-//         result,
-//       });
-//     })
-//     .catch((error) => {
-//       res
-//         .status(500)
-//         .send({ message: "profile information update save error", error });
-//     });
-// });
-
-// get profile
-// profileImage.get("/:email", async (req, res) => {
-//   try {
-//     const email = req.params.email;
-//     const query = { email: email };
-//     console.log(query);
-//     let result = await profiles.findOne(query);
-//     res.status(200).send({
-//       message: "the profile information ",
-//       status: 200,
-//       result,
-//     });
-//   } catch (err) {
-//     res.status(500).send({ message: "the resume get single data error ", err });
-//   }
-// });
-
-module.exports = profileImage;
+module.exports = profileImageRouter;
