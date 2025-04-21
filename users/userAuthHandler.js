9971364e358d10ed1a0d158a73f813233c6e5382
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-04-21 22:56:37 +0600

    the change the get signle user find the block

--- users/userAuthHandler.js
+++ users/userAuthHandler.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const userRouter = express.Router();
 const User = require("../model_schemas/userSchema");
 
@@ -24,15 +25,33 @@ userRouter.get("/", async (req, res) => {
   res.status(200).send({ message: "the get successfully !", result });
 });
 // block check kra
+// userRouter.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+
+//   await User.findOne(query)
+//     .then((result) => {
+//       res.status(200).send({ message: "The user find successfully", result });
+//     })
+//     .catch((error) => {
+//       res.status(500).send({ massage: "Error Delete user:", error });
+//     });
+// });
+
 userRouter.get("/:email", async (req, res) => {
-  const email = req.params.email;
-  await User.deleteOne({ email: email })
-    .then((result) => {
-      res.status(200).send({ message: "The user find successfully", result });
-    })
-    .catch((error) => {
-      res.status(500).send({ massage: "Error Delete user:", error });
+  try {
+    const id = req.params.email;
+    const query = { email: id };
+    let result = await User.findOne(query);
+    res.status(200).send({
+      message: "the user block find successfully ",
+      status: 200,
+      result,
     });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "the user data get single data error ", err });
+  }
 });
 
 // delete user working
