d79b53df3edea59319a93b5a150e862ec270443a
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-04-29 01:16:13 +0600

    the add import resume

---
+++ importResumeManage/resumeImportHandle.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const resumeInRouter = express.Router();
+const resumeIn = require("../model_schemas/resumeImportSchema");
+
+// post resume working
+// resumeInRouter.post("/", async (req, res) => {
+//   const resumeInformation = req.body;
+//   const resumeModel = new resumeInfo(resumeInformation);
+//   await resumeModel
+//     .save()
+//     .then((result) => {
+//       console.log("resume save");
+//       res
+//         .status(200)
+//         .send({ message: "resume information save", status: 200, result });
+//     })
+//     .catch((error) => {
+//       console.log("resume ", error);
+//       res.status(500).send({ message: "resume information save error", error });
+//     });
+// });
+
+// update resume working
+// resumeInRouter.put("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   let updateInformation = req.body;
+//   const resumeModel = new resumeInfo();
+//   const query = { _id: id };
+//   console.log(updateInformation, query);
+//   let updateInfo = {
+//     $set: {
+//       ...updateInformation,
+//     },
+//   };
+//   await resumeInfo
+//     .updateOne(query, updateInfo)
+//     .then((result) => {
+//       console.log(" update resume save");
+//       res.status(200).send({
+//         message: "resume information update save",
+//         status: 200,
+//         result,
+//       });
+//     })
+//     .catch((error) => {
+//       console.log("resume ", error);
+//       res
+//         .status(500)
+//         .send({ message: "resume information update save error", error });
+//     });
+// });
+
+// delete user working
+// resumeInRouter.delete("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await resumeInfo
+//     .deleteOne({ _id: id })
+//     .then((result) => {
+//       res
+//         .status(200)
+//         .send({ message: "The resume Delete successfully", status: 200 });
+//     })
+//     .catch((error) => {
+//       res.status(500).send({ massage: "Error Delete resume:", error });
+//     });
+// });
+
+// get all resume relate working
+// resumeInRouter.get("/", async (req, res) => {
+//   try {
+//     let result = await resumeInfo.find({});
+//     res
+//       .status(200)
+//       .send({ message: "the resume get successfully ", status: 200, result });
+//   } catch (err) {
+//     res.status(500).send({ message: "the resume get error ", err });
+//   }
+// });
+
+// get single resume relate working
+// resumeInRouter.get("/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const query = { _id: id };
+//     console.log(query);
+//     let result = await resumeInfo.findOne(query);
+//     res.status(200).send({
+//       message: "the resume get single data successfully ",
+//       status: 200,
+//       result,
+//     });
+//   } catch (err) {
+//     res.status(500).send({ message: "the resume get single data error ", err });
+//   }
+// });
+
+module.exports = resumeInRouter;
---
+++ model_schemas/resumeImportSchema.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const resumeInSchema = new Schema({
+  personalInfo: {
+    fullName: String,
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: String,
+    address: String,
+    jobTitle: String,
+    profileImage: String,
+    imageName: String,
+    about: String,
+    date: {
+      type: Date,
+    },
+    gender: String,
+    nationality: String,
+  },
+  education: [
+    {
+      school: String,
+      degree: String,
+      field: String,
+      startDate: {
+        type: Date,
+        required: true,
+      },
+      endDate: {
+        type: Date,
+        required: true,
+      },
+      grade: String,
+      description: String,
+    },
+  ],
+  skills: [String],
+  socialLinks: [
+    {
+      platform: String,
+      link: String,
+    },
+  ],
+  projects: [
+    {
+      projectName: String,
+      description: String,
+      live: String,
+      client: String,
+      server: String,
+      features: [String],
+    },
+  ],
+  experience: [
+    {
+      company: String,
+      position: String,
+      startMonth: String,
+      endMonth: String,
+      description: String,
+    },
+  ],
+  languages: [
+    {
+      language: String,
+      proficiency: String,
+    },
+  ],
+});
+
+const resumeIn = mongoose.model("ResumeImport", resumeInSchema);
+
+module.exports = resumeIn;
