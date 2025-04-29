6991fb3d72f751e1fac0826c37289e0ddd32209b
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-04-29 14:18:18 +0600

    create api update and delete

--- importResumeManage/resumeImportHandle.js
+++ importResumeManage/resumeImportHandle.js
@@ -23,49 +23,48 @@ resumeInRouter.post("/", async (req, res) => {
 });
 
 // update resume working
-// resumeInRouter.put("/:id", async (req, res) => {
-//   const id = req.params.id;
-//   let updateInformation = req.body;
-//   const resumeModel = new resumeInfo();
-//   const query = { _id: id };
-//   console.log(updateInformation, query);
-//   let updateInfo = {
-//     $set: {
-//       ...updateInformation,
-//     },
-//   };
-//   await resumeInfo
-//     .updateOne(query, updateInfo)
-//     .then((result) => {
-//       console.log(" update resume save");
-//       res.status(200).send({
-//         message: "resume information update save",
-//         status: 200,
-//         result,
-//       });
-//     })
-//     .catch((error) => {
-//       console.log("resume ", error);
-//       res
-//         .status(500)
-//         .send({ message: "resume information update save error", error });
-//     });
-// });
+resumeInRouter.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  let updateInformation = req.body;
+  const query = { _id: id };
+  console.log(updateInformation, query);
+  let updateInfo = {
+    $set: {
+      ...updateInformation,
+    },
+  };
+  await resumeIn
+    .updateOne(query, updateInfo)
+    .then((result) => {
+      res.status(200).send({
+        message: "resume import file information update save",
+        status: 200,
+        result,
+      });
+    })
+    .catch((error) => {
+      console.log("resume ", error);
+      res
+        .status(500)
+        .send({ message: "resume information update save error", error });
+    });
+});
 
 // delete user working
-// resumeInRouter.delete("/:id", async (req, res) => {
-//   const id = req.params.id;
-//   await resumeInfo
-//     .deleteOne({ _id: id })
-//     .then((result) => {
-//       res
-//         .status(200)
-//         .send({ message: "The resume Delete successfully", status: 200 });
-//     })
-//     .catch((error) => {
-//       res.status(500).send({ massage: "Error Delete resume:", error });
-//     });
-// });
+resumeInRouter.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await resumeIn
+    .deleteOne({ _id: id })
+    .then((result) => {
+      res.status(200).send({
+        message: "The resume import file Delete successfully",
+        status: 200,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({ massage: "Error Delete resume:", error });
+    });
+});
 
 // get all resume relate working
 resumeInRouter.get("/", async (req, res) => {
@@ -86,7 +85,6 @@ resumeInRouter.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const query = { _id: id };
-    console.log(query);
     let result = await resumeIn.findOne(query);
     res.status(200).send({
       message: "the resume import file get single data successfully ",
